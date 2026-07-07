18722210952b9e3da79a6dc88f00c1f4b988eb5c
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2026-07-07 22:16:37 +0200

    Initialize SvelteKit project

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
+
+export default config;
