b7770a94c6e5b08093d74ea471c0b46c6c6844c2
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2026-07-15 13:45:04 +0200

    Use node-adapter

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
